06dfd6e08e767dd7976916e682704d0627925a38
sampathReddy222 <87116669+sampathReddy222@users.noreply.github.com>
2026-04-07 12:14:58 +0530

    Add functionality to print container data
    Prints all container data from 'SC_Models_Scope'. Collects column names and formats row output.

---
+++ samplePYCODE.py
@@ -0,0 +1,14 @@
+Print all container data
+colms = []
+cont = Product.GetContainerByName("SC_Models_Scope").Rows
+for row in cont:
+    for col in row.Columns:
+        Trace.Write(col.Name)
+        colms.append(str(col.Name))
+    break
+for row in cont:
+    rowString = ""
+    for col in colms:
+        rowString += (str(col) + "	==	" + (row[col] if row[col] not in [None,""] else "") + "		")
+    Trace.Write(str(rowString))
+=================================================================================================================
